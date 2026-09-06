3fe659358ccc69cce609ba6e24b0b355df1401c1
RohithL4681 <105497480+RohithL4681@users.noreply.github.com>
2026-09-06 19:13:05 +0530

    chore: update src/_data/gallery.js

--- src/_data/gallery.js
+++ src/_data/gallery.js
@@ -1,12 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-
 const captions = {
   'chatgpt-image-aug-13-2026-01-16-35-pm.jpg': 'make it yourself Life of Life',
   'hash-tag-screen.jpg': 'Digital summit',
   'images-2.jpg': 'Plant a tree',
   'images-3.jpg': 'Happy weekend',
-  'images.jpg': 'Food donation for childern',
+  'images.jpg': 'Food donation for childerns',
 };
 
 module.exports = function () {
